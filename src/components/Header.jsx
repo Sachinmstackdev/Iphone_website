9eeb35c1b8980a26fa3c417f5c81b4797eed619b
sachin kumar <109363151+Sachinmstackdev@users.noreply.github.com>
2024-10-14 10:43:18 +0530

    Initial commit for iphone website

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <header>
+      <img src="/assets/images/apple.svg" alt="Apple logo" />
+      {/* ... other header content ... */}
+    </header>
+  );
+}
---
+++ src/components/YourComponent.jsx
@@ -0,0 +1,1 @@
+<img src="/assets/images/apple.svg" alt="Apple logo" />
